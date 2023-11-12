6dd67aa7d6ee9c916cfda23d70f4d29f94c93ee8
Tam Hug <270007579@yoobeestudent.ac.nz>
2023-11-12 18:06:55 +1300

    Appbar completed - Work on menu

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -8,25 +8,80 @@ class homePage extends StatelessWidget {
     return Scaffold(
 
       appBar: AppBar(
+        toolbarHeight: 100, //increase hight of the appbar
         title: const Text(
           'SignZ',
           style: TextStyle(
+            fontFamily: 'KottaOne', //changing the font style
             color: Colors.white,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+            fontSize: 40, //font size to make it clearer
+            //fontWeight: FontWeight.bold,
           ),
         ),
-        flexibleSpace: Container(
+        flexibleSpace: Container( // setting the background image for the appbar
             decoration: const BoxDecoration(
                 image: DecorationImage(
-                    image: AssetImage('assets/images/appBar04.jpg'),
-                    fit: BoxFit.cover)),
+                    image: AssetImage('assets/images/appBar04.jpg'), //Setting the image for background
+                    fit: BoxFit.cover)),//making sure it fit and wasn't distorted
           ),
-        centerTitle: true,
+        centerTitle: true, //making the title center of the widget
 
       ),
+      drawer: const NavigationDrawer(),
 
 
     );
   }
+}
+
+//creating a navigation menu
+class NavigationDrawer extends StatelessWidget {
+  const NavigationDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) => Drawer(
+    child: SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          buildHeader(context),
+          buildMenuItems(context),
+        ]
+      ),
+    )
+
+  );
+
+  Widget buildHeader(BuildContext context) => Container(
+    padding: EdgeInsets.only(
+      top: MediaQuery.of(context).padding.top,
+    ),
+  
+  );
+  Widget buildMenuItems(BuildContext context) => Column(
+    children: [  
+    ListTile(
+         leading: const Icon(Icons.insert_comment_outlined),
+         title: const Text('Main Search'),
+         onTap: () {},
+        ),
+      
+      ListTile(
+       leading: const Icon(Icons.home_outlined),
+       title: const Text('Name Search'),
+       onTap: () {},
+      ),
+       ListTile(
+       leading: const Icon(Icons.home_outlined),
+       title: const Text('Information'),
+       onTap: () {},
+      ),
+       ListTile(
+       leading: const Icon(Icons.home_outlined),
+       title: const Text('Home'),
+       onTap: () {},
+      ),
+      ],
+   );
+  
 }
