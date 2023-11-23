54a32f2d150bbde54a86bdeae3bb23b557ec0998
Tam Hug <270007579@yoobeestudent.ac.nz>
2023-11-23 23:26:36 +1300

    Name Search UI
    Progress

--- lib/pages/name_page.dart
+++ lib/pages/name_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:tam01/models/letterModel.dart';
+import 'package:tam01/pages/home_pages.dart';
+import 'package:tam01/pages/info_page.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -16,10 +20,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'SignZ'),
     );
   }
 }
@@ -81,10 +84,30 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+         toolbarHeight: 100, //increase hight of the appbar
+        title:  Text(
+          widget.title,
+          style: TextStyle(
+            fontFamily: 'KottaOne', //changing the font style
+            color: Colors.white,
+            fontSize: 40, //font size to make it clearer
+            //fontWeight: FontWeight.bold,
+          ),
+        ),
+        flexibleSpace: Container( // setting the background image for the appbar
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/appBar04.jpg'), //Setting the image for background
+                    fit: BoxFit.cover)),//making sure it fit and wasn't distorted
+          ),
+        centerTitle: true, //making the title center of the widget
+
       ),
+
+       drawer: const NavigationDrawer(),
+        backgroundColor: Color.fromRGBO(212, 239, 230, 1),
+       // title: Text(widget.title),
+      
       body: Center(
        
         child: Padding(
@@ -105,18 +128,20 @@ class _MyHomePageState extends State<MyHomePage> {
            
               hintText:"Search Name",
             ),
+            SizedBox(height: 20),
 
             Expanded(
               child: ListView.builder(
                 itemCount: items.isEmpty ? allItems.length : items.length,
                 itemBuilder: (context, index) {
-                  final item = items.isEmpty ? allItems[index] : items[index].letter;
+                  final itemL = items.isEmpty ? allItems[index] : items[index].letter;
             
                   return Card(
                     child: Column(
                       children: [
-                        Text('Letter: $item'),
-                        SizedBox(height: 8),
+                        Text('$itemL'),
+                        SizedBox(height: 20),
+                        SvgPicture.asset(allItems[index].imagePath),
                       ],
                     ),
                   );
@@ -133,4 +158,76 @@ class _MyHomePageState extends State<MyHomePage> {
      
     );
   }
+}
+
+//creating a navigation menu
+class NavigationDrawer extends StatelessWidget {
+  const NavigationDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) => Drawer(
+    child: SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          buildHeader(context),
+          buildMenuItems(context),
+        ]
+      ),
+    )
+
+  );
+
+  Widget buildHeader(BuildContext context) => Container(
+    padding: EdgeInsets.only(
+      top: MediaQuery.of(context).padding.top,
+    ),
+  
+  );
+  Widget buildMenuItems(BuildContext context) => Column(
+    children: [  
+    ListTile(
+         leading: const Icon(Icons.insert_comment_outlined),
+         title: const Text('Main Search'),
+         onTap: () {},
+        ),
+      
+         ListTile(
+       leading: const Icon(Icons.person_outline_outlined),
+       title: const Text('Name Search'),
+       onTap: () {
+        //Navigator.pushReplacement(
+       // context,MaterialPageRoute(builder: (context) => namePage (), //Navigating to the information page
+       // ),
+        //);
+       },
+        ),
+
+
+       ListTile(
+       leading: const Icon(Icons.info_outline),
+       title: const Text('Information'),
+       onTap: () {
+        Navigator.pushReplacement(
+        context,MaterialPageRoute(builder: (context) => infoPage (), //Navigating to the information page
+        ),
+        );
+
+
+       },
+      ),
+       ListTile(
+       leading: const Icon(Icons.home_outlined),
+       title: const Text('Home'),
+       onTap: () {
+         Navigator.pushReplacement(
+         context,MaterialPageRoute(builder: (context) => homePage (), //Navigating to the main home page
+         ),
+        );
+       },
+      ),
+      ],
+   );
+   
+  
 }
