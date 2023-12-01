ecf782964e2f1d5ea5aade72eac0f7521b66e8d8
Tam Hug <270007579@yoobeestudent.ac.nz>
2023-12-02 00:24:46 +1300

    Troubleshooting errors
    Fixed the bug on the name search and tried and failed to fix the errors on the main search page

--- lib/pages/name_page.dart
+++ lib/pages/name_page.dart
@@ -5,8 +5,6 @@ import 'package:tam01/pages/info_page.dart';
 import 'package:tam01/pages/name_page.dart';
 import 'package:tam01/pages/mSearch_page.dart';
 
-
-
 void main() {
   runApp(const namePage());
 }
@@ -17,11 +15,9 @@ class namePage extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       title: 'SignZ',
       debugShowCheckedModeBanner: false,
-     
       home: const MyNamePage(title: 'SignZ'),
     );
   }
@@ -29,7 +25,6 @@ class namePage extends StatelessWidget {
 
 class MyNamePage extends StatefulWidget {
   const MyNamePage({super.key, required this.title});
-
 
   final String title;
 
@@ -44,13 +39,12 @@ class _MyHomePageState extends State<MyNamePage> {
 
   final TextEditingController searchController = TextEditingController();
 
-  void _getLetters(){
+  void _getLetters() {
     allItems = letterModel.getLetters();
   }
 
-@override
+  @override
   void initState() {
-    
     super.initState();
     searchController.addListener(queryListener);
   }
@@ -58,24 +52,27 @@ class _MyHomePageState extends State<MyNamePage> {
   void queryListener() {
     search(searchController.text);
   }
-@override
+
+  @override
   void dispose() {
     searchController.removeListener(queryListener);
     searchController.dispose();
     super.dispose();
   }
-  void search(String query){
-    if (query.isEmpty){
+
+  void search(String query) {
+    if (query.isEmpty) {
       setState(() {
         items = allItems;
       });
-    }
-    else {
+    } else {
       setState(() {
-        items = allItems.where((e) => e.letter.contains(query.toUpperCase())).toList();
+        items = allItems
+            .where((e) => e.letter.contains(query.toUpperCase()))
+            .toList();
       });
-
-    };
+    }
+    ;
   }
 
   @override
@@ -84,8 +81,8 @@ class _MyHomePageState extends State<MyNamePage> {
 
     return Scaffold(
       appBar: AppBar(
-         toolbarHeight: 100, //increase hight of the appbar
-        title:  Text(
+        toolbarHeight: 100, //increase hight of the appbar
+        title: Text(
           widget.title,
           style: TextStyle(
             fontFamily: 'KottaOne', //changing the font style
@@ -94,87 +91,80 @@ class _MyHomePageState extends State<MyNamePage> {
             //fontWeight: FontWeight.bold,
           ),
         ),
-        flexibleSpace: Container( // setting the background image for the appbar
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/images/appBar04.jpg'), //Setting the image for background
-                    fit: BoxFit.cover)),//making sure it fit and wasn't distorted
-          ),
+        flexibleSpace: Container(
+          // setting the background image for the appbar
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(
+                      'assets/images/appBar04.jpg'), //Setting the image for background
+                  fit: BoxFit.cover)), //making sure it fit and wasn't distorted
+        ),
         centerTitle: true, //making the title center of the widget
-
       ),
 
-       drawer: const NavigationDrawer(),
-        backgroundColor: Color.fromRGBO(212, 239, 230, 1),
-       // title: Text(widget.title),
-      
+      drawer: const NavigationDrawer(),
+      backgroundColor: Color.fromRGBO(212, 239, 230, 1),
+      // title: Text(widget.title),
+
       body: Center(
-       
         child: Padding(
           padding: EdgeInsets.only(left: 8, right: 8),
           child: Column(
-          
-          children: <Widget>[
-           
-            SizedBox(height: 15),
-            SearchBar(
-              controller: searchController,
-              leading: IconButton(
-               onPressed: () {
-
-               }, 
-               icon: Icon(Icons.search),
-              ),
-           
-              hintText:"Search Name",
-            ),
-            SizedBox(height: 20),
-
-            Expanded(
-              child: ListView.builder(
-                itemCount: items.isEmpty ? allItems.length : items.length,
-                itemBuilder: (context, index) {
-                  final itemL = items.isEmpty ? allItems[index] : items[index].letter;
-                  final itemD = items.isEmpty ? allItems[index] : items[index].description;
-                 
-            
-                  return Card(
-                    
-                    child: Column(
-
-                      children: [
-                        SizedBox(height: 10),
-                        Image.asset(items[index].imagePath), //Displaying image
-                        
-                        ListTile(
-                          leading: SizedBox(height: 10),
-                          title:  Text( //Showing letter
-                            '                        $itemL\n', 
-                            style:TextStyle(
-                              fontSize: 20,
-                            ),
-                            ),
-                            //Showing Description
-                          subtitle:Text(
-                            '$itemD\n',
-                            textAlign: TextAlign.center,
-                            ),
-                          ),
-                      ],
-                    ),
-                    
-                  );
-                },
-                
+            children: <Widget>[
+              SizedBox(height: 15),
+              SearchBar(
+                controller: searchController,
+                leading: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.search),
                 ),
-            ),
-            
-           
-          ],
+                hintText: "Search Name",
+              ),
+              SizedBox(height: 20),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: items.isEmpty ? allItems.length : items.length,
+                  itemBuilder: (context, index) {
+                    final itemL =
+                        items.isEmpty ? allItems[index] : items[index].letter;
+                    final itemD = items.isEmpty
+                        ? allItems[index]
+                        : items[index].description;
+
+                    return items.isEmpty
+                        ? Card()
+                        : Card(
+                            child: Column(
+                              children: [
+                                SizedBox(height: 10),
+                                Image.asset(
+                                    items[index].imagePath), //Displaying image
+
+                                ListTile(
+                                  leading: SizedBox(height: 10),
+                                  title: Text(
+                                    //Showing letter
+                                    '                        $itemL\n',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  //Showing Description
+                                  subtitle: Text(
+                                    '$itemD\n',
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
-     
     );
   }
 }
@@ -185,73 +175,74 @@ class NavigationDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Drawer(
-    child: SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          buildHeader(context),
-          buildMenuItems(context),
-        ]
-      ),
-    )
-
-  );
+          child: SingleChildScrollView(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildHeader(context),
+              buildMenuItems(context),
+            ]),
+      ));
 
   Widget buildHeader(BuildContext context) => Container(
-    padding: EdgeInsets.only(
-      top: MediaQuery.of(context).padding.top,
-    ),
-  
-  );
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top,
+        ),
+      );
   Widget buildMenuItems(BuildContext context) => Column(
-    children: [  
-    ListTile(
-         leading: const Icon(Icons.insert_comment_outlined),
-         title: const Text('Main Search'),
-          onTap: () {
-        Navigator.pushReplacement(
-        context,MaterialPageRoute(builder: (context) => mainSPage (), //Navigating to the information page
-        ),
-        );
-       },
-        ),
-      
-         ListTile(
-       leading: const Icon(Icons.person_outline_outlined),
-       title: const Text('Name Search'),
-       onTap: () {
-        Navigator.pushReplacement(
-        context,MaterialPageRoute(builder: (context) => namePage (), //Navigating to the information page
-        ),
-        );
-       },
-        ),
-
-
-       ListTile(
-       leading: const Icon(Icons.info_outline),
-       title: const Text('Information'),
-       onTap: () {
-        Navigator.pushReplacement(
-        context,MaterialPageRoute(builder: (context) => infoPage (), //Navigating to the information page
-        ),
-        );
-
-
-       },
-      ),
-       ListTile(
-       leading: const Icon(Icons.home_outlined),
-       title: const Text('Home'),
-       onTap: () {
-         Navigator.pushReplacement(
-         context,MaterialPageRoute(builder: (context) => homePage (), //Navigating to the main home page
-         ),
-        );
-       },
-      ),
-      ],
-   );
-   
-  
+        children: [
+          ListTile(
+            leading: const Icon(Icons.insert_comment_outlined),
+            title: const Text('Main Search'),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      mainSPage(), //Navigating to the information page
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.person_outline_outlined),
+            title: const Text('Name Search'),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      namePage(), //Navigating to the information page
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.info_outline),
+            title: const Text('Information'),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      infoPage(), //Navigating to the information page
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Home'),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      homePage(), //Navigating to the main home page
+                ),
+              );
+            },
+          ),
+        ],
+      );
 }
